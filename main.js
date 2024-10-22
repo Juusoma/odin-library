93c076d743db2c00fff8680d833fce7a58dc5365
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-22 19:20:21 +0300

    Add book button

--- main.js
+++ main.js
@@ -17,5 +17,39 @@ function addBookToLibrary() {
     const pages = prompt("page count: ");
     const read = false;
     const book = new Book(title, author, pages, read);
-    console.log("Added book to library: \n", book.info());
+    myLibrary.push(book);
+    return book;
+    //console.log("Added book to library: \n", book.info());
 }
+
+const libraryElement = document.querySelector(".library-container")
+const newBookButtonElement = document.querySelector("#button-new-book");
+newBookButtonElement.addEventListener("click", () => {
+    const book = addBookToLibrary();
+    createElementForBook(book);
+});
+
+function createElementForBook(book){
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.innerHTML = `
+         <div class="book-spine">
+            <h2 class="book-heading">${book.title}</h2>
+        </div>
+        <div class="book-front-cover">
+            <h2 class="book-front-title">${book.title}</h2>
+            <p class="book-front-author">${book.author}</p>
+            <p class="book-front-pages">Pages: ${book.pages}</p>
+        </div>
+    `;
+    libraryElement.appendChild(bookElement);
+}
+
+function createElementsForAllBooks(){
+    for(const book in myLibrary){
+        createElementForBook(book);
+    }
+}
+
+const book_0 = new Book("Pirates of Hollow", "Jolly Roger", 533, false);
+createElementForBook(book_0);
